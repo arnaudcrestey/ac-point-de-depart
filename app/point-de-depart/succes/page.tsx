aead7641366c3a4da829c55ae18b2b2f443e79b6
arnaudcrestey <arnaud.crestey14@gmail.com>
2026-04-22 22:36:14 +0200

    Create page.tsx

---
+++ app/point-de-depart/succes/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div style={{ textAlign: "center", padding: 40 }}>
+      <h1>Merci</h1>
+      <p>Votre formulaire a bien été transmis.</p>
+    </div>
+  );
+}
